7039133318c9f8d19fe0b6edded22fb4061cd3ae
ico277 <enfrico123@gmail.com>
2022-12-30 19:36:36 +0100

    modified code

--- src/main.c
+++ src/main.c
@@ -1,1 +1,26 @@
-int main() {}
+#include <stdio.h>
+#include <string.h>
+
+FILE *HISTFILE = NULL;
+int LIST_COUNT = 10;
+
+int main(int argc, char** argv) {
+	for(int i = 1; i < argc; i++) {
+		char *arg = argv[i];
+		if(!strncmp(arg, "-f", 2)) {
+			if (argc - 1 < i) {
+				HISTFILE = fopen(arg + 1, "r");
+				if (HISTFILE == NULL) {
+					fprintf(stderr, "Error: invalid file '%s'!", arg + 1);
+				}
+			}
+		} else if (!strncmp(arg, "-c", 2)) {
+			if (argc - 1 < i) {
+				
+			}
+		}
+	}
+	if (HISTFILE == NULL) {
+		fprintf(stderr, "Error: no histfile given!");
+	}
+}
